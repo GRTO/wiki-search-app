fb42de0fa37403cf319fb4a886b223edcc70f9b5
Gerson Ossio <gossio@evolution.com>
2023-07-27 12:14:23 +0100

    Adding search loading page and error

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import getWikiResult from "@/lib/getWikiResult";
+import Item from "./components/Item";
 
 type Props = {
   params: {
     searchTerm: string;
   };
 };
+
+export async function generateMetadata(props: Props) {
+  const {
+    params: { searchTerm },
+  } = props;
+
+  const wikiData: Promise<SearchResult> = getWikiResult(searchTerm);
+  const data = await wikiData;
+
+  const displayTerm = searchTerm.replaceAll("%20", " ");
+
+  if (!data?.query?.pages) {
+    return {
+      title: `${displayTerm} Not Found`,
+    }
+  }
+
+  return {
+    title: displayTerm,
+    description: `Search results for ${displayTerm}`,
+  }
+}
 
 export default async function SearchResult(props: Props) {
   const {
@@ -14,14 +37,14 @@ export default async function SearchResult(props: Props) {
 
   const wikiData: Promise<SearchResult> = getWikiResult(searchTerm);
   const data = await wikiData;
-  console.log("Data", data);
+
   const results: Result[] | undefined = data?.query?.pages;
 
   const content = (
     <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
       {results ? (
         Object.values(results).map((result) => (
-          <p key={result.pageid}>{JSON.stringify(result)}</p>
+          <Item key={result.pageid} result={result} />
         ))
       ) : (
         <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
